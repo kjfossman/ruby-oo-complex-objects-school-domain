feade19b946bb8e1ef63cc0f283ecf1f1fdaf212
Kyle <kjfossman@gmail.com>
2021-02-09 15:56:49 -0900

    commit tree 759afe82a293c874099ba0978fb5ab5aed86bd30

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,40 @@
 # code here!
+require 'pry'
+
+class School
+    attr_accessor :roster, :add_student
+    def initialize(roster)
+        @roster = {}
+    end 
+    def add_student=(student, grade)
+       @student = name
+        @grade = grade
+    
+    end
+    def add_student(student, grade)
+        if @roster[grade] == nil 
+        @roster[grade] = [student]
+        else 
+            @roster[grade] << student
+        end
+        #@roster = {grade => [student]}    #binding.pry
+    end
+
+    def grade(year)
+        return @roster[year]
+    end
+
+    def sort
+         sorted = {}
+         @roster.each do |grade, students|
+            sorted[grade] = students.sort
+            
+           
+         end
+       
+        return sorted
+    end
+
+
+
+end
